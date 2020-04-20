84f015dc8bd72ed7f52ae314cc1977b790042638
Cupcake <meowcats2121@gmail.com>
2020-04-20 01:03:15 -0700

    Fixed drill issue, added 7 safes that spawn amount varies per difficulty, fixed some wrong interaction data with some objects, added missing loc

--- mc_jewlerystore/hooks/InteractionTweakData.lua
+++ mc_jewlerystore/hooks/InteractionTweakData.lua
@@ -3,7 +3,7 @@ Hooks:PostHook(InteractionTweakData, "init", "init_jewelry_int", function(self, 
 	self.hold_place_cobblestone = {
 		text_id = "hud_int_hold_cobblestone",
 		action_text_id = "hud_action_craft",
-		timer = 1,
+		timer = 0.25,
 		start_active = false,
 		special_equipment = "cobblestone",
 		equipment_text_id = "hud_equipment_need_cobblestone",
@@ -12,10 +12,11 @@ Hooks:PostHook(InteractionTweakData, "init", "init_jewelry_int", function(self, 
 		sound_done = "bar_insert_printingplates_finished",
 		equipment_consume = true
 	}
+	
 	self.hold_place_stick = {
 		text_id = "hud_int_hold_stick",
 		action_text_id = "hud_action_craft",
-		timer = 1,
+		timer = 0.25,
 		start_active = false,
 		special_equipment = "stick",
 		equipment_text_id = "hud_equipment_need_stick",
@@ -25,10 +26,36 @@ Hooks:PostHook(InteractionTweakData, "init", "init_jewelry_int", function(self, 
 		equipment_consume = true
 	}	
 	
+	self.hold_place_sand = {
+		text_id = "hud_int_hold_sand",
+		action_text_id = "hud_action_craft",
+		timer = 0.25,
+		start_active = false,
+		special_equipment = "sand",
+		equipment_text_id = "hud_equipment_need_sand",
+		sound_start = "bar_insert_printingplates",
+		sound_interupt = "bar_insert_printingplates_cancel",
+		sound_done = "bar_insert_printingplates_finished",
+		equipment_consume = true
+	}
+	
+	self.hold_place_gunpowder = {
+		text_id = "hud_int_hold_gunpowder",
+		action_text_id = "hud_action_craft",
+		timer = 0.25,
+		start_active = false,
+		special_equipment = "gunpowder",
+		equipment_text_id = "hud_equipment_need_gunpowder",
+		sound_start = "bar_insert_printingplates",
+		sound_interupt = "bar_insert_printingplates_cancel",
+		sound_done = "bar_insert_printingplates_finished",
+		equipment_consume = true
+	}
+	
 	self.hold_place_ladder = {
 		text_id = "hud_int_hold_ladder",
 		action_text_id = "hud_action_place_block",
-		timer = 1,
+		timer = 0.25,
 		start_active = false,
 		special_equipment = "ladder",
 		equipment_text_id = "hud_equipment_need_ladder",
@@ -41,7 +68,7 @@ Hooks:PostHook(InteractionTweakData, "init", "init_jewelry_int", function(self, 
 	self.hold_place_lever = {
 		text_id = "hud_int_hold_lever",
 		action_text_id = "hud_action_place_block",
-		timer = 1,
+		timer = 2,
 		start_active = false,
 		special_equipment = "lever",
 		equipment_text_id = "hud_equipment_need_lever",
@@ -51,23 +78,20 @@ Hooks:PostHook(InteractionTweakData, "init", "init_jewelry_int", function(self, 
 		equipment_consume = true
 	}
 	
-	self.hold_place_drill = {
-		text_id = "hud_int_hold_drill",
-		action_text_id = "hud_action_place_block",
-		timer = 1,
-		start_active = false,
+	self.drill = {
+		icon = "equipment_drill",
+		contour = "interactable_icon",
+		text_id = "hud_int_equipment_drill",
+		equipment_text_id = "hud_int_equipment_no_drill",
 		special_equipment = "drill",
+		equipment_consume = true,
 		equipment_text_id = "hud_equipment_need_drill",
-		sound_start = "bar_insert_printingplates",
-		sound_interupt = "bar_insert_printingplates_cancel",
-		sound_done = "bar_insert_printingplates_finished",
-		equipment_consume = true
-	}
-	
-	self.use_lever = {
-		text_id = "hud_int_use_lever",
-		action_text_id = "hud_action_use_lever",
-		timer = 0.5,
-		start_active = false
+		timer = 3,
+		blocked_hint = "no_drill",
+		sound_start = "bar_drill_apply",
+		sound_interupt = "bar_drill_apply_cancel",
+		sound_done = "bar_drill_apply_finished",
+		axis = "y",
+		action_text_id = "hud_action_placing_drill"
 	}
 end)
