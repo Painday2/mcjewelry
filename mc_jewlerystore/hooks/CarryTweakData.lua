b61ff1a39af7e0c23b0980beb2b0a5ef94d75712
Mako <masterjon902@gmail.com>
2020-04-21 04:31:30 -0500

    part 2 of custom bags, added filter to loot, stopped random items from spawning ontop of diamond

--- mc_jewlerystore/hooks/CarryTweakData.lua
+++ mc_jewlerystore/hooks/CarryTweakData.lua
@@ -3,6 +3,7 @@ Hooks:PostHook(CarryTweakData, "init", "init_minecraftoff", function(self, tweak
 		type = "light",
 		name_id = "hud_carry_gold_block",
 		bag_value = "diamonds",
+		unit = "units/pd2_mod_craft/props/prop_cube_gold_block",
 		visual_unit_name = "units/pd2_mod_craft/props/prop_cube_gold_block",
 		AI_carry = {
 			SO_category = "enemies"
@@ -13,6 +14,7 @@ Hooks:PostHook(CarryTweakData, "init", "init_minecraftoff", function(self, tweak
 		type = "light",
 		name_id = "hud_carry_diamond_block",
 		bag_value = "diamonds",
+		unit = "units/pd2_mod_craft/props/prop_cube_diamond_block",
 		visual_unit_name = "units/pd2_mod_craft/props/prop_cube_diamond_block",
 		AI_carry = {
 			SO_category = "enemies"
@@ -23,6 +25,7 @@ Hooks:PostHook(CarryTweakData, "init", "init_minecraftoff", function(self, tweak
 		type = "light",
 		name_id = "hud_carry_emerald_block",
 		bag_value = "diamonds",
+		unit = "units/pd2_mod_craft/props/prop_cube_emerald_block",
 		visual_unit_name = "units/pd2_mod_craft/props/prop_cube_emerald_block",
 		AI_carry = {
 			SO_category = "enemies"
