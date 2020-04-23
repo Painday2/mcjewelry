fee67258b6883b600e8cd0fdc2fe818b6b35c50c
Mako <masterjon902@gmail.com>
2020-04-22 23:26:17 -0500

    sync is fixed :Crabrave:

--- mc_jewlerystore/hooks/CarryTweakData.lua
+++ mc_jewlerystore/hooks/CarryTweakData.lua
@@ -4,7 +4,7 @@ Hooks:PostHook(CarryTweakData, "init", "init_minecraftoff", function(self, tweak
 		name_id = "hud_carry_gold_block",
 		bag_value = "diamonds",
 		unit = "units/pd2_mod_craft/props/prop_cube_gold_block",
-		visual_unit_name = "units/pd2_mod_craft/props/prop_cube_gold_block",
+		visual_unit_name = "units/pd2_mod_craft/props/prop_cube_gold_block_acc",
 		AI_carry = {
 			SO_category = "enemies"
 		}
@@ -15,7 +15,7 @@ Hooks:PostHook(CarryTweakData, "init", "init_minecraftoff", function(self, tweak
 		name_id = "hud_carry_diamond_block",
 		bag_value = "diamonds",
 		unit = "units/pd2_mod_craft/props/prop_cube_diamond_block",
-		visual_unit_name = "units/pd2_mod_craft/props/prop_cube_diamond_block",
+		visual_unit_name = "units/pd2_mod_craft/props/prop_cube_diamond_block_acc",
 		AI_carry = {
 			SO_category = "enemies"
 		}
@@ -26,7 +26,7 @@ Hooks:PostHook(CarryTweakData, "init", "init_minecraftoff", function(self, tweak
 		name_id = "hud_carry_emerald_block",
 		bag_value = "diamonds",
 		unit = "units/pd2_mod_craft/props/prop_cube_emerald_block",
-		visual_unit_name = "units/pd2_mod_craft/props/prop_cube_emerald_block",
+		visual_unit_name = "units/pd2_mod_craft/props/prop_cube_emerald_block_acc",
 		AI_carry = {
 			SO_category = "enemies"
 		}
